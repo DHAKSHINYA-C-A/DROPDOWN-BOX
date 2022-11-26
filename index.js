9f1b4c44cde6e5547740075f9e1a0baec4b4f58e
DHAKSHINYA-C-A <118709379+DHAKSHINYA-C-A@users.noreply.github.com>
2022-11-26 04:33:31 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Pro from './project.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Pro />
+  </React.StrictMode>
+);
---
+++ project.js
@@ -0,0 +1,78 @@
+import { React, useState } from 'react';
+import Select from 'react-select';
+import './dropdown.css';
+function Pro ()
+{
+    const colors = [
+      {
+        id: 1,
+        label: "lime",
+    },
+    {
+        id: 2,
+        label: "lavender",
+    },
+    {
+        id: 3,
+        label: "crimson",
+    },
+    {
+        id: 4,
+        label: "darkblue",
+    },
+    {
+        id: 5,
+        label: "teal",
+    },
+    {
+        id: 6,
+        label: "rebeccapurple",
+    },
+    {
+        id: 7,
+        label: "ghostwhite",
+    },
+    {
+        id: 8,
+        label: "aquamarine",
+    },
+    {
+        id: 9,
+        label: "aliceblue",
+    },
+    ]
+
+    const [color, setColor] = useState("");
+
+    const click = (event) => {
+        setColor(event.label);
+    }
+    return (
+        <div className='dropdownWrapper'>
+            <style>
+            {
+                `.button 
+                {
+                 background-color:` + color + ';'
+                }
+            </style>
+
+            <center>
+            <h1>COLOR CHANGER</h1>
+            </center>
+            <div className="block">
+                <center>
+                    <button className='button'></button>
+                </center>
+            </div><br></br>
+            
+            <center><div className="dropdownContainer">
+                <Select options={colors} onChange={click} className='select' placeholder="Select Color"></Select>
+            </div></center>
+            
+            
+        </div>
+
+    );
+}
+export default Pro;
